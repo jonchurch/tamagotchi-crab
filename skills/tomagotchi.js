a56443699865d7c82a243724aba5e0d051905d4f
jonchurch <me@jonchurch.com>
2017-10-26 12:56:38 -0400

    new Tomagotchi is born

---
+++ skills/tomagotchi.js
@@ -0,0 +1,20 @@
+
+// Create an instance of our pet
+const Tomagotchi= require('../lib/Tomagotchi')
+const pet = new Tomagotchi()
+
+module.exports = (controller) => {
+	controller.hears('^feed$', 'message_received, facebook_postback, quick_reply', (bot, message) => {
+		pet.feed()
+		bot.reply(message, 'Fed Pet')
+		bot.reply(message, pet.print())
+	})
+
+	controller.hears('^play$', 'message_received, facebook_postback, quick_reply', (bot, message) => {
+		pet.play()
+		bot.reply(message, 'Played with Pet')
+		bot.reply(message, pet.print())
+
+	})
+
+}
