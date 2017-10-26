d41ff88232e00a20ca9be2596f29844b823f02f9
jonchurch <me@jonchurch.com>
2017-10-26 18:38:19 -0400

    fix bored bug, add fave and hated foods

--- skills/tomagotchi.js
+++ skills/tomagotchi.js
@@ -1,13 +1,16 @@
 
+const emoji = require('node-emoji')
+
 // Create an instance of our pet
 const Tomagotchi= require('../lib/Tomagotchi')
 const pet = new Tomagotchi()
 
 module.exports = (controller) => {
 
-	controller.hears('^feed$', 'message_received, facebook_postback, quick_reply', (bot, message) => {
-		pet.feed()
-		bot.reply(message, 'Fed Pet')
+	controller.hears(['^feed$', `^${emoji.get('apple')}$`, `^${emoji.get('ramen')}$`], 'message_received, facebook_postback, quick_reply', (bot, message) => {
+		const food = emoji.which(message.text)
+
+		bot.reply(message, pet.feed(food))
 		bot.reply(message, {
 			text: pet.print(),
 			quick_replies: pet.quick_menu()
@@ -25,6 +28,7 @@ module.exports = (controller) => {
 	})
 
 	controller.hears('(.*)', 'message_received, facebook_postback, quick_reply', (bot, message) => {
+		console.log(`Heard: ${message.text}`)
 		bot.reply(message, {
 			text: pet.idle(), 
 			quick_replies: pet.quick_menu()
