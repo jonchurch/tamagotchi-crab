3493109ca2bef6115b6f748b8c789071d26a368b
jonchurch <me@jonchurch.com>
2017-10-27 19:32:36 -0400

    return images instead of text, upload gifs

--- skills/tomagotchi.js
+++ skills/tomagotchi.js
@@ -4,14 +4,41 @@ const emoji = require('node-emoji')
 // Create an instance of our pet
 const Tomagotchi= require('../lib/Tomagotchi')
 const pet = new Tomagotchi()
+var images = []
 
 module.exports = (controller) => {
+	controller.api.attachment_upload.upload({
+			type: 'image',
+			payload: {
+				url: 'https://media.giphy.com/media/KaGQJvlsMqupa/giphy.gif',
+				is_reusable: true
+			}
+		}, (err, attachmentId) => {
+			images['pet'] = attachmentId
+})
+	controller.api.attachment_upload.upload({
+			type: 'image',
+			payload: {
+				url: 'https://m.popkey.co/52621a/Wpkmg_s-200x150.gif',
+				is_reusable: true
+			}
+		}, (err, attachmentId) => {
+			images['nom'] = attachmentId
+})
 
 	controller.hears(['^feed$', ...emoji.food ], 'message_received, facebook_postback, quick_reply', async (bot, message) => {
 		const food = emoji.which(message.text)
 
-		await bot.reply(message, pet.feed(food))
+		pet.feed(food)
+
+		bot.reply(message, {attachment: {
+			type: 'image',
+			payload: {
+				attachment_id: images['nom']
+			}
+		}})
 		console.log(pet.print())
+		console.log({images})
 	})
 
 	controller.hears('^play$', 'message_received, facebook_postback, quick_reply', async (bot, message) => {
@@ -24,9 +51,16 @@ module.exports = (controller) => {
 	})
 
 	controller.hears('(.*)', 'message_received, facebook_postback, quick_reply', (bot, message) => {
-
+		pet.idle(message.text)
 		console.log(pet.print())
-		bot.reply(message, pet.idle(message.text))
+		console.log(images['pet'])
+		bot.reply(message, {attachment: {
+			type: 'image',
+			payload: {
+				attachment_id: images['pet']
+			}
+		}})
+		// bot.reply(message, )
 	})
 
 }
