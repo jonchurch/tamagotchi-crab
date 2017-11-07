da7ce903615dcd7582b0e960b114bbfbf0ed0a11
jonchurch <me@jonchurch.com>
2017-11-07 02:09:06 -0500

    return messages, kill pets

--- skills/tamagotchi.js
+++ skills/tamagotchi.js
@@ -11,25 +11,27 @@ module.exports = (controller) => {
 	controller.hears(['^feed$', ...emoji.food ], 'message_received, facebook_postback, quick_reply', (bot, message) => {
 		// this is only grabbing the first occurence of the first emoji in our list above
 		const food = message.match[0]
-		pet.feed(food)
+		const msg = pet.feed(food)
 
-		bot.reply(message, {attachment: {
-			type: 'image',
-			payload: {
-				url: controller.images.nom
-			}
-		}})
+		// bot.reply(message, {attachment: {
+		// 	type: 'image',
+		// 	payload: {
+		// 		url: controller.images.nom
+		// 	}
+		// }})
 		bot.reply(message, pet.print())
+		bot.reply(message, msg)
 		console.log(pet.print())
 	})
 
 	controller.hears(['^play$', ...emoji.activity ], 'message_received, facebook_postback, quick_reply', (bot, message) => {
-		pet.play()
+		const msg = pet.play()
 		console.log(pet.print())
 		bot.reply(message, {
 			text: emoji.get('balloon'),
 		})
 		bot.reply(message, pet.print())
+		bot.reply(message, msg)
 
 	})
 
@@ -40,16 +42,17 @@ module.exports = (controller) => {
 	})
 
 	controller.hears('(.*)', 'message_received, facebook_postback, quick_reply', (bot, message) => {
-		pet.idle(message.text)
+		const msg = pet.idle(message.text)
 		console.log(pet.print())
-		bot.reply(message, {attachment: {
-			type: 'image',
-			payload: {
-				url: controller.images.pet
-			}
-		}})
+		// bot.reply(message, {attachment: {
+		// 	type: 'image',
+		// 	payload: {
+		// 		url: controller.images.pet
+		// 	}
+		// }})
+		bot.reply(message, msg)
 		bot.reply(message, pet.print())
-		pet.happy >= 90 ? bot.reply(message, emoji.get(':heart:')) : null
+		// pet.happy >= 90 ? bot.reply(message, emoji.get(':heart:')) : null
 
 	})
 
